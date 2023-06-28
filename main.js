bd14013ec873afd9f96891002bbcb02968330bd9
정호재 <hjjeong@langcode.io>
2023-06-28 17:21:26 +0900

    search까지의 기능 추가

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let news = [];
 let menus = document.querySelectorAll(".menus button");
 menus.forEach(x => x.addEventListener("click", (event)=>getNewsByTopic(event) ));
+
+let searchButton = document.getElementById("search-button");
+
 // const getLatestNews = async ()=>{
 //     let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`
 //                     );
@@ -36,7 +39,7 @@ const getLatestNews = async ()=>{
 const getNewsByTopic = async (event) => {
     console.log("클릭됨", event.target.textContent);
     let topic = event.target.textContent.toLowerCase();
-    let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${topic}&pageSize=10&apiKey=64bd66b165fa4d0799cc58d64f61e6d2`);
+    let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${topic}&pageSize=20&apiKey=64bd66b165fa4d0799cc58d64f61e6d2`);
     
     let response = await fetch(url); //ajax, http, fetch
     let data = await response.json();
@@ -48,6 +51,25 @@ const getNewsByTopic = async (event) => {
     render();
 
 }
+
+const getNewsByKeyword = async () => {
+    //1. 검색 키워드 읽어오기
+    //2. url에 검색 키워드 붙이기
+    //3. 헤더 준비
+    //4. url 부르기
+    //5. 데이터 가져오기
+    //6. 데이터 보여주기
+
+    let keyword = document.getElementById("search-input").value;
+    let url = new URL(`https://newsapi.org/v2/everything?q=${keyword}&apiKey=64bd66b165fa4d0799cc58d64f61e6d2`);
+
+    let response = await fetch(url);
+    let data = await response.json();
+    news = data.articles;
+
+    render();
+
+};
 
 const render = () => {
     let newsHTML = '';
@@ -73,4 +95,6 @@ const render = () => {
     document.getElementById("news-board").innerHTML = newsHTML;
 }
 
+
+searchButton.addEventListener("click", getNewsByKeyword);
  getLatestNews();
